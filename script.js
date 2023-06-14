1e65edbc46527a62fe0b9892ae676b6a88401dcd
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-06-14 14:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-function numberOfBooksRead(library){
-	count 0;
-}
-{
-	author:"bill gates";
-	title:"The road ahead";
-	readingStatus:"true";
-}
-{
-	author:"shivaji sawant";
-	title:"mrityunjay";
-	readingStatus:"false";
-}
-{
-	author:"jay shetty";
-	title:"The monk who sold his ferrari";
-	readingStatus:"true";
-}
+const library =[
+	
 
-const numberOfBooksRead = () => {
+{
+	author:"bill gates",
+	title:"The road ahead",
+	readingStatus:"true",
+},
+{
+	author:"shivaji sawant",
+	title:"mrityunjay",
+	readingStatus:"false",
+},
+{
+	author:"jay shetty",
+	title:"The monk who sold his ferrari",
+	readingStatus:"true",
+},
+]
+const numberOfBooksRead = () => 
+	let count=0;
+for(let i=0 ; i<library.length;i++){
+	library[i].readingStatus&&count++;
+}
+return count;
   // write your code here
 };
 
